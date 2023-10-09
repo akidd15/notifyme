96f187838793de2ff6c69a12903a15bb94dba9ee
Allison Farris <allison.perry98@gmail.com>
2023-10-09 13:14:35 -0600

    added post request

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const app = express();
 const PORT = process.env.PORT||3001
 const path = require('path');
 const notes = require("./db/db.json")
+const fs = require("fs");
+const { v4: uuidv4} = require("uuid");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -17,9 +19,33 @@ res.sendFile(path.join(__dirname, "public/notes.html"))
     //res.sendFile(path.join(__dirname, "notes.html"))
 //})
 
-//app.post("/api/notes", (req, res) => {
-  //  res.json()
-//})
+app.get('/api/notes', (req, res) => {
+    fs.readFile("./db/db.json", (err, data) => {
+        if (err) {
+            console.log(err)
+        } else return res.json(JSON.parse(data))
+    }
+)});
+
+app.post("/api/notes", (req, res) => {
+    const { title, text } = req.body;
+    if (req.body) {
+        const newNote = {
+            title,
+            text,
+            note_id: uuidv4()
+        };
+        fs.readFile("./db/db.json", "utf-8", (err, data) => {
+            if (err) {
+                console.error(err);
+            } else {
+                const parsedData = JSON.parse(data);
+                parsedData.push(newNote);
+                fs.writeFile("./db/db.json", JSON.stringify(parsedData, null, 4), (err) => err ? console.error(err) : console.log("done"))
+            }
+        })
+    }
+})
 
 app.listen(PORT, () => 
 console.log(`Listening on ${PORT}`))
